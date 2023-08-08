47eb3bc4736f96fba109222e1480f3495c4982ac
olaf <olaf@olafui-MacBookAir.local>
2023-08-08 18:20:58 +0900

    feat: 비디오 컴포넌트 추가

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -6,7 +6,17 @@ import Section from '../shared/Section'
 const cx = classNames.bind(styles)
 
 function Video() {
-  return <Section className={cx('container')}>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video
+        src="/assets/main.mp4"
+        autoPlay
+        loop
+        muted
+        poster="/assets/poster.jpg"
+      />
+    </Section>
+  )
 }
 
 export default Video
