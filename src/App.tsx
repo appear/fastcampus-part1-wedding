64d59239d5a1311c68a90d72647dc62f8b2dbee7
olaf <olaf@olafui-MacBookAir.local>
2023-08-11 00:15:34 +0900

    feat: 지도 구현

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import ImageGallery from './components/sections/ImageGallery'
 import Intro from './components/sections/Intro'
 import Invitation from './components/sections/Invitation'
 import Calendar from './components/sections/Calendar'
+import Map from './components/sections/Map'
 
 const cx = classNames.bind(styles)
 
@@ -78,6 +79,7 @@ function App() {
       <Invitation message={invitation} />
       <ImageGallery images={galleryImages} />
       <Calendar date={date} />
+      <Map location={location} />
       {JSON.stringify(wedding)}
     </div>
   )
